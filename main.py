b6c567465edf898f2965dc71e1a21b9a9b2912d6
lauradiaz <lauradiaz1586@gmail.com>
2021-04-15 11:10:37 +0400

    calculate the total inventory value per supplier. Logic products with inventory less than 10. Add column with values for the total price. Create a new excel sheet with the new column added.

--- main.py
+++ main.py
@@ -8,24 +8,55 @@ product_list = inv_file["Sheet1"]
 
 # Calculate how many products I have per supplier and list the name of
 # the suppliers with that respective number of products.
-prod_per_supplier = {}
+products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
 
 print(product_list.max_row)
 # Read how many products are in the list to iterate.
 # start from 2 and end in the last row (including)
 for product_row in range(2, product_list.max_row + 1):
    supplier_name = product_list.cell(product_row, 4).value
+   inventory = product_list.cell(product_row, 2).value
+   price = product_list.cell(product_row, 3).value
+   product_number = product_list.cell(product_row, 1).value
+   inventory_price = product_list.cell(product_row, 5)
 
-   if supplier_name in prod_per_supplier:
-      current_num_products = prod_per_supplier.get(supplier_name)
-      prod_per_supplier[supplier_name] = current_num_products + 1
+   # calculation for number of products per supplier:
+   if supplier_name in products_per_supplier:
+      current_num_products = products_per_supplier.get(supplier_name)
+      products_per_supplier[supplier_name] = current_num_products + 1
    else:
       print("Adding a new supplier")
-      prod_per_supplier[supplier_name] = 1
+      products_per_supplier[supplier_name] = 1
 
-print(prod_per_supplier)
+   # calculate the total inventory value per supplier:
+   if supplier_name in total_value_per_supplier:
+      current_total_value = total_value_per_supplier.get(supplier_name)
+      total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+   else:
+      total_value_per_supplier[supplier_name] = inventory * price
 
-# Calculate the total inventory value per supplier:
+   # logic products with inventory less than 10:
+   if inventory < 10:
+      products_under_10_inv[int(product_number)] = int(inventory)
+
+   # add value for total inventory price:
+   inventory_price.value = inventory * price
+
+
+print(products_per_supplier)
+print(total_value_per_supplier)
+print(products_under_10_inv)
+# save the file, it is going to create a new file:
+inv_file.save("inventory_with_total_value.xlsx")
+
+
+
+
+
+
+
 
 
 
